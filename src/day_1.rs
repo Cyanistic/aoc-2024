03b0ab606a48462b9ec3e7167edd6380327e658d
Cyanism <61608977+Cyanistic@users.noreply.github.com>
2024-12-01 14:44:25 -0500

    init

---
+++ src/day_1.rs
@@ -0,0 +1,11 @@
+use std::fs::read_to_string;
+
+pub fn part_1() -> usize {
+    let contents = read_to_string("input/day-1.txt").unwrap();
+    todo!()
+}
+
+pub fn part_2() -> usize {
+    let contents = read_to_string("input/day-1.txt").unwrap();
+    todo!()
+}
